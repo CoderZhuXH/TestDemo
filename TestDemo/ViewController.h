d8ea7c8ef6592a395d485a9a4ae41b2e5f63a4f4
Xiaohui Zhu <977950862@qq.com>
2016-12-22 21:32:01 +0800

    testDemo

---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by zhuxiaohui on 2016/12/22.
+//  Copyright © 2016年 it7090.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by zhuxiaohui on 2016/12/22.
+//  Copyright © 2016年 it7090.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
